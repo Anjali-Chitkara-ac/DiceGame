cc7b8f9d9af608c7669bb0044e59a2a343907eba
Anjali-Chitkara-ac <69366199+Anjali-Chitkara-ac@users.noreply.github.com>
2021-09-16 00:05:53 -0700

    adding basic logic

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -14,6 +14,71 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var imgOne: UIImageView!
+    
+    @IBOutlet weak var imgTwo: UIImageView!
+    
+    @IBOutlet weak var lblScore: UILabel!
+    
+    var score = 0
+    
+    @IBAction func below7pressed(_ sender: Any) {
+        let one = Int.random(in: 1...6)
+        let two = Int.random(in: 1...6)
+        
+        let imgOneName = "Dice\(one)"
+        let imgTwoName = "Dice\(two)"
+        
+        imgOne.image = UIImage(named: imgOneName)
+        imgTwo.image = UIImage(named: imgTwoName)
+        
+        if one + two < 7 {
+            score+=1
+        }else{
+            score-=1
+        }
+        
+        lblScore.text = "Score = \(score)"
+        
+    }
+    
+    @IBAction func lucly7pressed(_ sender: Any) {
+        let one = Int.random(in: 1...6)
+        let two = Int.random(in: 1...6)
+        
+        let imgOneName = "Dice\(one)"
+        let imgTwoName = "Dice\(two)"
+        
+        imgOne.image = UIImage(named: imgOneName)
+        imgTwo.image = UIImage(named: imgTwoName)
+        
+        if one + two == 7 {
+            score+=1
+        }else{
+            score-=1
+        }
+        
+        lblScore.text = "Score = \(score)"
+    }
+    
+    @IBAction func above7pressed(_ sender: Any) {
+        let one = Int.random(in: 1...6)
+        let two = Int.random(in: 1...6)
+        
+        let imgOneName = "Dice\(one)"
+        let imgTwoName = "Dice\(two)"
+        
+        imgOne.image = UIImage(named: imgOneName)
+        imgTwo.image = UIImage(named: imgTwoName)
+        
+        if one + two > 7 {
+            score+=1
+        }else{
+            score-=1
+        }
+        
+        lblScore.text = "Score = \(score)"
+    }
+    
 }
 
